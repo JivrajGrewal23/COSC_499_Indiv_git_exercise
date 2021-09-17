1ca667b475fd159dc2cadef929c5710525926192
JivrajGrewal23 <jivrajg@live.ca>
2021-09-16 21:58:24 -0700

    fixed int sort

--- intSort.py
+++ intSort.py
@@ -1,6 +1,14 @@
+# created a sorting method from lowest to highest int
 a = []
 def intSorted(a):
     return sorted(a)
 
+def intsorter(a):
+    if not a:
+        return []
+    return (intsorter([x for x in a[1:] if x <  a[0]])
+            + [a[0]] +
+            intsorter([x for x in a[1:] if x >= a[0]]))
+
 b = [4, 5, 1, -2 ,-3 ,4]
-print(intSorted(b))
+print(intsorter(b))
